c319fed4b3c52273a0e4ebe4ce4642f70193de51
jvandaal <jonas.vandaal@outlook.be>
2024-03-06 15:36:24 +0100

    feat: move to dotnet 8.0.2
    BREAKING CHANGE: move to dotnet 8.0.2

--- src/Be.Vlaanderen.Basisregisters.Utilities.HexByteConvertor/Properties/AssemblyInfo.cs
+++ src/Be.Vlaanderen.Basisregisters.Utilities.HexByteConvertor/Properties/AssemblyInfo.cs
@@ -5,8 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyProduct("Vlaamse basisregisters")]
 [assembly: AssemblyDescription("Easily convert between byte array and hex strings.")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
 [assembly: AssemblyCompany("Vlaamse overheid")]
 
